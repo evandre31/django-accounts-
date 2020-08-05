14bbd2d3463102992fc613b40a0fb2324d538144
foued <foued>
2020-08-05 19:55:15 +0200

    create profile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,4 +10,13 @@ class Profile(models.Model):
     address = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
-        return 'le profile de : '+str(self.user)
+        return 'le profile de : ' + str(self.user)
+
+
+# """pour créer automatiquement un profile"""
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
